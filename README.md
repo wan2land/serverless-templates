dfba99f50c35ac023de78a3e7d7b69ec30f73e9c
Cris <corgidisco@gmail.com>
2019-03-20 18:19:04 +0900

    update README, add pkg

--- README.md
+++ README.md
@@ -6,11 +6,19 @@
 
 ## AWS NodeJS Typescript
 
+- nodejs 8.10
 - serverless-webpack
 - serverless-offline
 - eslint (@typescript-eslint/eslint-plugin)
 
 ### Installation
+
+```bash
+sls create --template-url https://github.com/corgidisco/serverless-templates/tree/master/aws-nodejs-typescript --path myproject
+cd myproject
+npm install
+sls deploy --stage dev
+```
 
 ## License
 
--- aws-nodejs-typescript/src/handler.ts
+++ aws-nodejs-typescript/src/handler.ts
@@ -1,10 +1,15 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
 import "source-map-support/register"
 
+const pkg = require("../package.json") // eslint-disable-line
+
+
 export const hello: APIGatewayProxyHandler = async (event, ctx) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
+      name: pkg.name,
+      version: pkg.version,
       message: "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
       input: event,
     }),
